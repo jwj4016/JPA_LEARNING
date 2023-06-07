9065916557294ef9f0637a6b1061367e0bf0be1e
J <jwj4016@gmail.com>
2023-06-07 14:35:21 +0900

    entityManager 동작 방식과 영속성 컨텍스트에 대한 이해.

--- src/main/java/com/example/jpa_learning/entity/Member.java
+++ src/main/java/com/example/jpa_learning/entity/Member.java
@@ -6,10 +6,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Getter
 @Setter
 @Entity //이 클래스가 테이블과 매핑된다고 jpa 알려주는 어노테이션.(엔티티 클래스)
+//@DynamicUpdate 필드가 너무 많거나, 저장되는 내용이 너무 클 경우에 수정된 데이터만 사용해서 동적으로 UPDATE SQL 생성.
+//변경되는 필드에 따라 쿼리가 매번 변경되기 때문에 캐시 적중률이 낮아질 수도 있다.
+//일반적으로 컬럼이 30개 이상일 경우 동적 수동 쿼리가 더 빨라진다고 함.
+//하지만 본인의 환경에서 테스트를 시행하기.
 @Table(name="MEMBER", schema = "jpa")   //엔티티 클래스와 매핑할 테이블 정보 noti. 생략 시 클래스 이름을 테이블 이름으로 매핑.
 public class Member {
     @Id //테이블의 primary key와 매핑. 식별자 필드라고 함.
--- src/test/java/com/example/jpa_learning/EntityManagerTest.java
+++ src/test/java/com/example/jpa_learning/EntityManagerTest.java
@@ -1,10 +1,7 @@
 package com.example.jpa_learning;
 
 import com.example.jpa_learning.entity.Member;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.PersistenceUnit;
+import jakarta.persistence.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -39,6 +36,42 @@ public class EntityManagerTest {
         entityManagerFactory.close();
 
     }
+    @Test
+    void 엔티티를_영속성_컨텍스트에_등록하는_테스트() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        //entityManager.setFlushMode(FlushModeType.AUTO);   //커밋 or 쿼리 실행(JPQL 등) 시 플러시.(default)
+        //entityManager.setFlushMode(FlushModeType.COMMIT); //커밋할 때만 플러시.
+        //참고로 플러시는 영속성 컨텍스트에 보관된 엔티티를 지우는 것이 아니다.
+        EntityTransaction transaction = entityManager.getTransaction();
+        //엔티티 매니저는 데이터 변경 시 트랜잭션을 시작해야 한다.
+        transaction.begin();    //[트랜잭션] 시작
+
+        Member memberA = new Member();
+        memberA.setId("memberA");
+        memberA.setUsername("A");
+        memberA.setAge(1);
+
+        Member memberB = new Member();
+        memberB.setId("memberB");
+        memberB.setUsername("B");
+        memberB.setAge(32);
+
+        entityManager.persist(memberA);
+        entityManager.persist(memberB);
+        //여기까지는 INSERT SQL을 DB에 던지지 않는다.
+
+        //커밋하는 순간 데이터베이스에 INSERT SQL 보냄.
+        transaction.commit();   //[트랜잭션] 커밋
+    }
+
+    @Test
+    void testDetached() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        //준영속 상태
+        //entityManager.detach();
+        entityManager.clear();
+        entityManager.close();
+    }
 
     //비지니스 로직
     private static void logic(EntityManager entityManager) {
@@ -49,12 +82,14 @@ public class EntityManagerTest {
         member.setAge(35);
 
         //등록
+        //1차 캐시에 저장된다.
         entityManager.persist(member);
 
         //수정
         member.setAge(20);
 
         //한 건 조회
+        //1차 캐시에서 조회.
         Member findMember = entityManager.find(Member.class, id);
         System.out.println("findMemebr = " + findMember.getUsername() + ", age = " + findMember.getAge());
 
@@ -64,6 +99,71 @@ public class EntityManagerTest {
         System.out.println("members.size = " + memberList.size());
 
         //삭제
+        //영속성 컨텍스트에서도 제거된다.
+        //이렇게 삭제된 엔티티의 경우 재사용하지 않고 가비지 컬렉션의 대상이 되도록 놔두기.
         entityManager.remove(member);
     }
+
+    @Test
+    void mergeTest() {
+        Member member = createMember("memberA", "회원1");
+
+        member.setUsername("회원명변경");
+
+        mergeMember(member);
+
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        EntityTransaction transaction = entityManager.getTransaction();
+    }
+
+    private Member createMember(String id, String username) {
+        //영속성 컨텍스트1 시작
+        EntityManager em1 = entityManagerFactory.createEntityManager();
+        //동일 트랜잭션 내에서만 1차 캐시가 존재한다.
+        EntityTransaction tx1 = em1.getTransaction();
+        tx1.begin();
+
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername(username);
+        member.setAge(10);
+
+        em1.persist(member);
+        tx1.commit();
+
+        em1.close();    //영속성 컨텍스트1 종료, member 엔티티는 준영속 상태가 됨.
+        //영속성 컨텍스트1 종료.
+
+        //준영속 상태의 member entity 반환.
+        // 수정사항이 생겨도 db에 반영 불가.
+        return member;
+    }
+
+    private void mergeMember(Member member) {
+        //영속성 컨텍스트2 시작
+        EntityManager em2 = entityManagerFactory.createEntityManager();
+        EntityTransaction tx2 = em2.getTransaction();
+
+        tx2.begin();
+        //준영속 상태를 다시 영속 상태로 변경.
+        //준영속이 아닌 비영속 엔티티도 영속 상태로 변환가능.
+        //Member newMember = new Member();
+        //Member mergeMember = em.merge(newMember);
+        Member mergeMember = em2.merge(member);
+        tx2.commit();
+
+        //준영속 상태
+        System.out.println("member = " + member.getUsername());
+
+        //영속 상태
+        System.out.println("mergeMember = " + mergeMember.getUsername());
+
+        System.out.println("em2 contains member = " + em2.contains(member));
+
+        System.out.println("em2 contains mergeMember = " + em2.contains(mergeMember));
+
+        em2.close();
+        //영속성 컨텍스트2 종료
+    }
+
 }
